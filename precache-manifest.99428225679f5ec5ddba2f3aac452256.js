6910b9fc90aacb1a3a512b89b48f7d624b631238
Hudson Daniel <hudsonprem97@gmail.com>
2020-07-07 22:35:10 +0530

    Updates

---
+++ precache-manifest.99428225679f5ec5ddba2f3aac452256.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d02e6f9d7fb5067d7c8a484ec376b24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a81c58116d088b6bc80e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb970e9b53d8e5764da1",
+    "url": "/static/js/2.2b57b62f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b57b62f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a81c58116d088b6bc80e",
+    "url": "/static/js/main.dcdc75d9.chunk.js"
+  },
+  {
+    "revision": "9b0aa821c9c42531c382",
+    "url": "/static/js/runtime-main.5c9bfde8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
